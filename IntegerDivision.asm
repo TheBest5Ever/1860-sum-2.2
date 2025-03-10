7ed402e2f57f8b9a95476baaf008843a073f9ab3
TheBest5Ever <ll23rb@leeds.ac.uk>
2025-03-10 19:33:34 +0000

    doneee

--- IntegerDivision.asm
+++ IntegerDivision.asm
@@ -1,96 +1,111 @@
 @R2
 M=0
-
 @R3
 M=0
+@R4
+M=0
 
-@R0
+@R1
 D=M
-
-@DONE
+@ERROR
 D;JEQ
 
+@R0
+D=M
+@R5
+M=D
 @R1
 D=M
-
-R0
-D=M
-
-(NOTDONE)
-@R1
-D=D-M
-
-@R3
+@R6
 M=D
 
-@R2
-M=M+1
-
-@NOTDONE
-D;JLE
-
-
-
-
-
-
-@R2
-M=0  
-
-@R0
-D=M  
-
-@END
-D;JEQ  
-
-@R1
-D=D-M
-
-@END
+@R5
+D=M
+@NEGATIVE_NUMERATOR
 D;JLT
+@NEGATIVE_DENOMINATOR
+@R6
+D=M
+D;JLT
+@MAIN_LOOP
+0;JMP
 
-@R1
-D=M  
+(NEGATIVE_NUMERATOR)
+@R5
+M=-M
+@R6
+D=M
+@NEGATIVE_DENOMINATOR
+D;JLT
+@MAIN_LOOP
+0;JMP
 
-@UNDEFINED
-D;JEQ  
+(NEGATIVE_DENOMINATOR)
+@R6
+M=-M
+@MAIN_LOOP
+0;JMP
 
-(LOOP)
-    @R0
-    D=M  
-    @R1
-    D=D-M  // Subtract R1 from R0
+(MAIN_LOOP)
+    @R5
+    D=M
+    @R6
+    D=D-M
+    @END_DIVISION
+    D;JLT
 
-    @END  // If result < 0, stop the loop
-    D;JLT  
-
-    // Store new R0 value
-    @R0
-    M=D  
-
-    // Increment quotient (R2++)
     @R2
-    M=M+1  
+    M=M+1
 
-    @LOOP  // Repeat
-    0;JMP  
+    @R5
+    M=D
 
-// Handle division by zero case
-(DIV_BY_ZERO)
-    @R2
-    M=0  // Set quotient to 0
-    @R3
-    M=0  // Set remainder to 0
-    @EXIT
+    @MAIN_LOOP
     0;JMP
 
-// Store remainder in R3
-(END)
-    @R0
-    D=M  
+(END_DIVISION)
+    @R5
+    D=M
     @R3
-    M=D  // Store remainder
+    M=D
 
-(EXIT)
-    @EXIT
-    0;JMP  // Infinite loop to stop execution
+@R0
+D=M
+@RESTORE_REMAINDER_SIGN
+D;JLT
+@R1
+D=M
+@RESTORE_QUOTIENT_SIGN
+D;JLT
+@END
+0;JMP
+
+(RESTORE_REMAINDER_SIGN)
+    @R3
+    M=-M
+    @R1
+    D=M
+    @RESTORE_QUOTIENT_SIGN
+    D;JLT
+    @END
+    0;JMP
+
+(RESTORE_QUOTIENT_SIGN)
+    @R0
+    D=M
+    @R1
+    D=D*M
+    @R2
+    M=D
+    @END
+    0;JMP
+
+(ERROR)
+    @R4
+    M=1
+    @END
+    0;JMP
+
+(END)
+    @END
+    0;JMP
