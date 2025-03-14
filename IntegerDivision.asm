893c0909e620d69bf835f2029d5caef92f9f3fcd
TheBest5Ever <ll23rb@leeds.ac.uk>
2025-03-14 17:58:18 +0000

    rf# Please enter the commit message for your changes. Lines starting

--- IntegerDivision.asm
+++ IntegerDivision.asm
@@ -1,111 +1,102 @@
 @R2
 M=0
-@R3         //set up
+@R3
 M=0
 @R4
 M=0
 
+// Check if denominator is zero 
 @R1
-D=M         //check if denominator is 0 if so jumps to error
+D=M
 @ERROR
 D;JEQ
 
-@R0
+@R0     // Store numerator and denominator in temp registers
+
 D=M
 @R5
-M=D
+M=D      // Store numerator in R5
 @R1
-D=M             //store numerator and denominator
+D=M
 @R6
-M=D
+M=D     // Store denominator in R6
 
+// Check if numerator is negative
 @R5
 D=M
-@NEGATIVE_NUMERATOR         //jumps if numerator is negative
-D;JLT   
-@NEGATIVE_DENOMINATOR       //jumps if denominator is negative
+@NUM_NEGATIVE
+D;JLT  // If numerator < 0, go to NUM_NEGATIVE
+@CHECK_DEN_SIGN
+0;JMP
+
+(NUM_NEGATIVE)
+@R5
+M=-M  // Make numerator positive
+@R7
+M=1   // Set R7 = 1 (flag for negative numerator)
+
+(CHECK_DEN_SIGN)
+// Check if denominator is negative
 @R6
 D=M
-D;JLT
-@MAIN_LOOP
+@DEN_NEGATIVE
+D;JLT  
+@DIVISION_LOOP
 0;JMP
 
-(NEGATIVE_NUMERATOR)
-@R5                 //make numerator positive
-M=-M
+(DEN_NEGATIVE)
 @R6
+M=-M  // Make denominator positive
+@R8
+M=1   // Set R8 = 1 (flag for negative denominator)
+
+// Perform division using repeated subtraction
+(DIVISION_LOOP)
+@R5
 D=M
-@NEGATIVE_DENOMINATOR       
-D;JLT
-@MAIN_LOOP
-0;JMP
-
-(NEGATIVE_DENOMINATOR)
 @R6
-M=-M                    //make denominator positive
-@MAIN_LOOP
+D=D-M
+@END_DIVISION
+D;JLT  // If numerator < denominator, stop
+
+@R2
+M=M+1  // Increment quotient
+@R5
+M=D  // Update numerator
+@DIVISION_LOOP
 0;JMP
 
-(MAIN_LOOP)
-    @R5
-    D=M                         //subtracts denominator from numerator
-    @R6
-    D=D-M
-    @END_DIVISION               
-    D;JLT
-
-    @R2
-    M=M+1                       //quotientt increases by 1
-
-    @R5                         //stores new numerator
-    M=D
-
-    @MAIN_LOOP                  //loop repeats 
-    0;JMP
 
 (END_DIVISION)
-    @R5
-    D=M
-    @R3
-    M=D             //stores remainder
+@R5
+D=M
+@R3
+M=D  // Store remainder
 
-@R0
+// If numerator was negative, make remainder negative
+@R7
 D=M
-@RESTORE_REMAINDER_SIGN
-D;JLT                       //if numerator is negative remainder is negative
-@R1
+@END
+D;JEQ  // If R7 == 0  jump
+@R3
+M=-M  // Negate remainder
+
+// Determine quotient sign using R7 and R8 flags
+@R7
 D=M
-@RESTORE_QUOTIENT_SIGN
-D;JLT                       //if denominator is negative quotient is negative 
+@R8
+D=D-M  // D = R7 - R8
+@END
+D;JEQ  // If both signs were same, quotient is correct
+@R2
+M=-M  // Negate quotient
+
+(ERROR)
+@R4
+M=1  
 @END
 0;JMP
 
-(RESTORE_REMAINDER_SIGN)
-    @R3
-    M=-M                     
-    @R1
-    D=M
-    @RESTORE_QUOTIENT_SIGN
-    D;JLT
-    @END
-    0;JMP
-
-(RESTORE_QUOTIENT_SIGN)
-    @R0
-    D=M
-    @R1
-    D=D*M
-    @R2
-    M=D
-    @END
-    0;JMP
-
-(ERROR)
-    @R4
-    M=1
-    @END
-    0;JMP
-
 (END)
-    @END
-    0;JMP
+@END
+0;JMP
