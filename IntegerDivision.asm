7ee6c48ecac57251ba1c451c13954a9de41cb8c1
TheBest5Ever <ll23rb@leeds.ac.uk>
2025-03-14 17:20:00 +0000

    doneeeee

--- IntegerDivision.asm
+++ IntegerDivision.asm
@@ -1,12 +1,12 @@
 @R2
 M=0
-@R3
+@R3         //set up
 M=0
 @R4
 M=0
 
 @R1
-D=M
+D=M         //check if denominator is 0 if so jumps to error
 @ERROR
 D;JEQ
 
@@ -15,15 +15,15 @@ D=M
 @R5
 M=D
 @R1
-D=M
+D=M             //store numerator and denominator
 @R6
 M=D
 
 @R5
 D=M
-@NEGATIVE_NUMERATOR
-D;JLT
-@NEGATIVE_DENOMINATOR
+@NEGATIVE_NUMERATOR         //jumps if numerator is negative
+D;JLT   
+@NEGATIVE_DENOMINATOR       //jumps if denominator is negative
 @R6
 D=M
 D;JLT
@@ -31,58 +31,58 @@ D;JLT
 0;JMP
 
 (NEGATIVE_NUMERATOR)
-@R5
+@R5                 //make numerator positive
 M=-M
 @R6
 D=M
-@NEGATIVE_DENOMINATOR
+@NEGATIVE_DENOMINATOR       
 D;JLT
 @MAIN_LOOP
 0;JMP
 
 (NEGATIVE_DENOMINATOR)
 @R6
-M=-M
+M=-M                    //make denominator positive
 @MAIN_LOOP
 0;JMP
 
 (MAIN_LOOP)
     @R5
-    D=M
+    D=M                         //subtracts denominator from numerator
     @R6
     D=D-M
-    @END_DIVISION
+    @END_DIVISION               
     D;JLT
 
     @R2
-    M=M+1
+    M=M+1                       //quotientt increases by 1
 
-    @R5
+    @R5                         //stores new numerator
     M=D
 
-    @MAIN_LOOP
+    @MAIN_LOOP                  //loop repeats 
     0;JMP
 
 (END_DIVISION)
     @R5
     D=M
     @R3
-    M=D
+    M=D             //stores remainder
 
 @R0
 D=M
 @RESTORE_REMAINDER_SIGN
-D;JLT
+D;JLT                       //if numerator is negative remainder is negative
 @R1
 D=M
 @RESTORE_QUOTIENT_SIGN
-D;JLT
+D;JLT                       //if denominator is negative quotient is negative 
 @END
 0;JMP
 
 (RESTORE_REMAINDER_SIGN)
     @R3
-    M=-M
+    M=-M                     
     @R1
     D=M
     @RESTORE_QUOTIENT_SIGN
--- SumArrayEntries.asm
+++ SumArrayEntries.asm
@@ -1,38 +1,37 @@
 @R1
 D=M
-@CHECK_POSITIVE
+@CHECK_EMPTY        //check if the list is empty
 D;JLE
 
 @R2
 M=0
-
-@R3
+@R3         //sets sum and index counter 
 M=0
 
 @LOOP
 @R3
 D=M
 @R1
-D=D-M
+D=D-M           //check if index is less than the number of elements in the list
 @END_LOOP
 D;JGE
 
 @R0
 D=M
 @R3
-A=D+A
+A=D+A               //accesses the element at the index
 D=M
 
 @R2
-M=M+D
+M=M+D       //adds the current element in the array
 
 @R3
-M=M+1
+M=M+1           //increments index counter
 
 @LOOP
 0;JMP
 
-@CHECK_POSITIVE
+@CHECK_EMPTY
 0;JMP
 
 @END_LOOP
